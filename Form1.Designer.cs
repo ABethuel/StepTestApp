2d5f3256a54d9f70adf4c4fba15f7d106ec55bd7
Adrien Bethuel <adrien.bethuel@eleve.isep.fr>
2021-05-06 15:50:28 +0200

    display elements and error message corrections

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -42,6 +42,23 @@
             this.comboBoxHeight = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.grpBoxGender = new System.Windows.Forms.GroupBox();
+            this.radioButtonMale = new System.Windows.Forms.RadioButton();
+            this.radioButtonFemale = new System.Windows.Forms.RadioButton();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtBoxLvl1 = new System.Windows.Forms.TextBox();
+            this.txtBoxLvl2 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtBoxLvl3 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtBoxLvl4 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtBoxLvl5 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.grpBoxGender.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -159,7 +176,8 @@
             this.comboBoxHeight.Items.AddRange(new object[] {
             "15cm",
             "20cm",
-            "25cm"});
+            "25cm",
+            "30cm"});
             this.comboBoxHeight.Location = new System.Drawing.Point(701, 77);
             this.comboBoxHeight.Name = "comboBoxHeight";
             this.comboBoxHeight.Size = new System.Drawing.Size(113, 21);
@@ -172,24 +190,200 @@
             this.label6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(569, 77);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(104, 19);
+            this.label6.Size = new System.Drawing.Size(108, 19);
             this.label6.TabIndex = 12;
-            this.label6.Text = "Step Height :";
+            this.label6.Text = "Step Height  :";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(28, 170);
+            this.label7.Location = new System.Drawing.Point(28, 172);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(0, 13);
             this.label7.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(569, 114);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(57, 19);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Date  :";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(652, 113);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(162, 20);
+            this.dateTimePicker1.TabIndex = 15;
+            // 
+            // grpBoxGender
+            // 
+            this.grpBoxGender.Controls.Add(this.radioButtonFemale);
+            this.grpBoxGender.Controls.Add(this.radioButtonMale);
+            this.grpBoxGender.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpBoxGender.Location = new System.Drawing.Point(863, 59);
+            this.grpBoxGender.Name = "grpBoxGender";
+            this.grpBoxGender.Size = new System.Drawing.Size(200, 89);
+            this.grpBoxGender.TabIndex = 16;
+            this.grpBoxGender.TabStop = false;
+            this.grpBoxGender.Text = "Gender";
+            this.grpBoxGender.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // radioButtonMale
+            // 
+            this.radioButtonMale.AutoSize = true;
+            this.radioButtonMale.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonMale.Location = new System.Drawing.Point(15, 26);
+            this.radioButtonMale.Name = "radioButtonMale";
+            this.radioButtonMale.Size = new System.Drawing.Size(59, 20);
+            this.radioButtonMale.TabIndex = 0;
+            this.radioButtonMale.TabStop = true;
+            this.radioButtonMale.Text = "Male";
+            this.radioButtonMale.UseVisualStyleBackColor = true;
+            this.radioButtonMale.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged_1);
+            // 
+            // radioButtonFemale
+            // 
+            this.radioButtonFemale.AutoSize = true;
+            this.radioButtonFemale.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButtonFemale.Location = new System.Drawing.Point(15, 52);
+            this.radioButtonFemale.Name = "radioButtonFemale";
+            this.radioButtonFemale.Size = new System.Drawing.Size(73, 20);
+            this.radioButtonFemale.TabIndex = 1;
+            this.radioButtonFemale.TabStop = true;
+            this.radioButtonFemale.Text = "Female";
+            this.radioButtonFemale.UseVisualStyleBackColor = true;
+            this.radioButtonFemale.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(24, 192);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(70, 19);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Level 1 :";
+            // 
+            // txtBoxLvl1
+            // 
+            this.txtBoxLvl1.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxLvl1.Location = new System.Drawing.Point(100, 192);
+            this.txtBoxLvl1.Name = "txtBoxLvl1";
+            this.txtBoxLvl1.Size = new System.Drawing.Size(50, 21);
+            this.txtBoxLvl1.TabIndex = 18;
+            this.txtBoxLvl1.TextChanged += new System.EventHandler(this.txtBoxLvl1_TextChanged);
+            // 
+            // txtBoxLvl2
+            // 
+            this.txtBoxLvl2.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxLvl2.Location = new System.Drawing.Point(257, 192);
+            this.txtBoxLvl2.Name = "txtBoxLvl2";
+            this.txtBoxLvl2.Size = new System.Drawing.Size(50, 21);
+            this.txtBoxLvl2.TabIndex = 20;
+            this.txtBoxLvl2.TextChanged += new System.EventHandler(this.txtBoxLvl2_TextChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(181, 192);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(70, 19);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Level 2 :";
+            // 
+            // txtBoxLvl3
+            // 
+            this.txtBoxLvl3.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxLvl3.Location = new System.Drawing.Point(406, 192);
+            this.txtBoxLvl3.Name = "txtBoxLvl3";
+            this.txtBoxLvl3.Size = new System.Drawing.Size(50, 21);
+            this.txtBoxLvl3.TabIndex = 22;
+            this.txtBoxLvl3.TextChanged += new System.EventHandler(this.txtBoxLvl3_TextChanged);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(330, 192);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(70, 19);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Level 3 :";
+            // 
+            // txtBoxLvl4
+            // 
+            this.txtBoxLvl4.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxLvl4.Location = new System.Drawing.Point(565, 192);
+            this.txtBoxLvl4.Name = "txtBoxLvl4";
+            this.txtBoxLvl4.Size = new System.Drawing.Size(50, 21);
+            this.txtBoxLvl4.TabIndex = 24;
+            this.txtBoxLvl4.TextChanged += new System.EventHandler(this.txtBoxLvl4_TextChanged);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(489, 192);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(70, 19);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "Level 4 :";
+            // 
+            // txtBoxLvl5
+            // 
+            this.txtBoxLvl5.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxLvl5.Location = new System.Drawing.Point(723, 193);
+            this.txtBoxLvl5.Name = "txtBoxLvl5";
+            this.txtBoxLvl5.Size = new System.Drawing.Size(50, 21);
+            this.txtBoxLvl5.TabIndex = 26;
+            this.txtBoxLvl5.TextChanged += new System.EventHandler(this.txtBoxLvl5_TextChanged);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(647, 193);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(70, 19);
+            this.label13.TabIndex = 25;
+            this.label13.Text = "Level 5 :";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(863, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 30);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "Validate";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.ClientSize = new System.Drawing.Size(1075, 672);
+            this.ClientSize = new System.Drawing.Size(1080, 679);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtBoxLvl5);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.txtBoxLvl4);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.txtBoxLvl3);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtBoxLvl2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtBoxLvl1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.grpBoxGender);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.comboBoxHeight);
@@ -207,6 +401,8 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpBoxGender.ResumeLayout(false);
+            this.grpBoxGender.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -228,6 +424,22 @@
         private System.Windows.Forms.ComboBox comboBoxHeight;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.GroupBox grpBoxGender;
+        private System.Windows.Forms.RadioButton radioButtonMale;
+        private System.Windows.Forms.RadioButton radioButtonFemale;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtBoxLvl1;
+        private System.Windows.Forms.TextBox txtBoxLvl2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtBoxLvl3;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtBoxLvl4;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtBoxLvl5;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -28,11 +28,9 @@ namespace StepTestApp
 
         private void txtBoxAge_TextChanged(object sender, EventArgs e)
         {
-            
             try
             {
                 float age = (float)double.Parse(txtBoxAge.Text);
-                double variable = Convert.ToDouble(age);
                 double MaxHr = age + 220;
                 double Hr = MaxHr * 0.85;
                 lblMaxHR.Text = MaxHr.ToString();
@@ -42,27 +40,20 @@ namespace StepTestApp
             }
             catch
             {
-                DialogResult errorString;
-                errorString = MessageBox.Show("Please enter correct value", "Warning error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                lblMaxHR.Text = "/";
-                lblHR.Text = "/";
+                if (txtBoxAge.Text == "")
+                {
+                    lblMaxHR.Text = "/";
+                    lblHR.Text = "/";
+                }
+                else
+                {
+                    DialogResult errorString;
+                    errorString = MessageBox.Show("Please enter a correct value", "Warning error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    lblMaxHR.Text = "/";
+                    lblHR.Text = "/";
+                    txtBoxAge.Text = "";
+                }       
             }
-
-            /*if (txtBoxAge.Text == "")
-            {
-                lblMaxHR.Text = "/";
-                lblHR.Text = "/";
-            }
-            else 
-            {
-                float age = (float)double.Parse(txtBoxAge.Text);
-                double MaxHr = age + 220;
-                double Hr = MaxHr * 0.85;
-                lblMaxHR.Text = MaxHr.ToString();
-                lblHR.Text = Hr.ToString();
-                lblB1.Text = "b/min";
-                lblB2.Text = "b/min";
-            }*/
         }
 
         private void lblMaxHR_Click(object sender, EventArgs e)
@@ -88,6 +79,141 @@ namespace StepTestApp
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBoxLvl1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                float lvl1 = (float)double.Parse(txtBoxLvl1.Text);
+            }
+            catch
+            {
+                if (txtBoxLvl1.Text == "")
+                {
+                    txtBoxLvl1.Text = "";
+                }
+                else
+                {
+                    DialogResult errorString;
+                    errorString = MessageBox.Show("Please enter a correct value", "Warning error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtBoxLvl1.Text = "";
+                }
+            }
+        }
+
+        private void txtBoxLvl2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                float lvl2 = (float)double.Parse(txtBoxLvl2.Text);
+            }
+            catch
+            {
+                if (txtBoxLvl2.Text == "")
+                {
+                    txtBoxLvl2.Text = "";
+                }
+                else
+                {
+                    DialogResult errorString;
+                    errorString = MessageBox.Show("Please enter a correct value", "Warning error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtBoxLvl2.Text = "";
+                }
+            }
+        }
+
+        private void txtBoxLvl3_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                float lvl3 = (float)double.Parse(txtBoxLvl3.Text);
+            }
+            catch
+            {
+                if (txtBoxLvl3.Text == "")
+                {
+                    txtBoxLvl1.Text = "";
+                }
+                else
+                {
+                    DialogResult errorString;
+                    errorString = MessageBox.Show("Please enter a correct value", "Warning error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtBoxLvl3.Text = "";
+                }
+            }
+        }
+
+        private void txtBoxLvl4_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                float lvl4 = (float)double.Parse(txtBoxLvl4.Text);
+            }
+            catch
+            {
+                if (txtBoxLvl4.Text == "")
+                {
+                    txtBoxLvl4.Text = "";
+                }
+                else
+                {
+                    DialogResult errorString;
+                    errorString = MessageBox.Show("Please enter a correct value", "Warning error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtBoxLvl4.Text = "";
+                }
+            }
+        }
+
+        private void txtBoxLvl5_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                float lvl5 = (float)double.Parse(txtBoxLvl5.Text);
+            }
+            catch
+            {
+                if (txtBoxLvl5.Text == "")
+                {
+                    txtBoxLvl5.Text = "";
+                }
+                else
+                {
+                    DialogResult errorString;
+                    errorString = MessageBox.Show("Please enter a correct value", "Warning error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtBoxLvl5.Text = "";
+                }
+            }
         }
     }
 }
